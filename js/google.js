0a2e6e58c23310f1ee4e4541c2430c9e3c3b9154
Andrés Maldonado <contact.a.maldonado@gmail.com>
2020-10-02 15:27:09 -0300

    update

--- js/google.js
+++ js/google.js
@@ -1,13 +1,19 @@
 const logo = document.getElementById('hplogo');
 const body = document.getElementById('gsr');
 const devandmus = document.createElement('div');
-devandmus.innerHTML = 'Devandmus';
-devandmus.style = '';
+devandmus.innerHTML = 'DEVANDMUS';
+devandmus.style = 'font-weight:900;font-family: "Poppins",sans-serif;color:white;font-size:40px;padding-top:100px!important';
 
 // trigger
-(function(){
-  logo.parentElement.insertBefore(devandmus, logo);
-})()
 window.onload = () => {
-  logo.parentElement.insertBefore(devandmus, logo);
+    logo.parentElement.insertBefore(devandmus, logo);
+    logo.remove();
+
+    body.style.backgroundImage = 'linear-gradient(to right, #008C83, #008DCD)';
+
+    document.querySelector('.gLFyf.gsfi').placeholder = 'Ándale ándale ándale!!!';
+
+    while (document.querySelector('[value="Buscar con Google"]')) {
+        document.querySelector('[value="Buscar con Google"]').value = 'Búscar con tu amigo @devandmus';
+    }
 }
